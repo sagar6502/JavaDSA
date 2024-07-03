c4f60ff7df1d49092fb194ab41725899b5d371e2
sagar6502 <ssunar@bayonesolutions.com>
2024-07-03 17:54:36 +0530

    DLL

--- src/linkedlists/Main.java
+++ src/linkedlists/Main.java
@@ -2,19 +2,35 @@ package linkedlists;
 
 public class Main {
 	public static void main(String[] args) {
-		LL list = new LL();
+//		LL list = new LL();
+//		list.insertFirst(3);
+//		list.insertFirst(1);
+//		list.insertFirst(7);
+//		list.insertLast(9);
+//		list.display();
+//		list.delLast1();
+//		list.insertLast1(99);
+//		list.delFirst();
+//		list.insert(100, 2);
+//		list.display();
+//		list.deleteAtIndex(1);
+//		list.display();
+//		System.out.println(list.getIndex(100));
+		
+		DLL list = new DLL();
 		list.insertFirst(3);
-		list.insertFirst(1);
-		list.insertFirst(7);
-		list.insertLast(9);
+		list.insertFirst(6);
+		list.insertFirst(9);
+		list.insertFirst(13);
+		list.insertFirst(17);
+		list.insertFirst(91);
 		list.display();
-		list.delLast1();
-		list.insertLast1(99);
-		list.delFirst();
-		list.insert(100, 2);
+		list.insertLast(7);
+//		list.insertLast(79);
 		list.display();
-		list.deleteAtIndex(1);
+		list.insertAtIndex(6,100);
 		list.display();
-		System.out.println(list.getIndex(100));
+//		list.delAtIndex(6);
+//		list.display();
 	}
 }
