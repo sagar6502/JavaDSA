fd9a6ac2e58311524bbfdc5ac505449bb8d74dce
sagar6502 <sagarsunar202@gmail.com>
2024-09-08 06:01:46 +0530

    graph implemented using adjacency list

---
+++ src/graphDataStructure/Graph.java
@@ -0,0 +1,77 @@
+package graphDataStructure;
+
+import java.util.ArrayList;
+
+public class Graph {
+
+	/*
+	 * Graph is a non-linear data structure consisting of vertices and edges. The
+	 * vertices are sometimes also referred to as nodes and the edges are lines or
+	 * arcs that connect any two nodes in the graph. More formally a Graph is
+	 * composed of a set of vertices( V ) and a set of edges( E ). The graph is
+	 * denoted by G(V, E).
+	 * 
+	 * Components of a Graph:
+	 * 
+	 * Vertices: Vertices are the fundamental units of the graph. Sometimes,
+	 * vertices are also known as vertex or nodes. Every node/vertex can be labeled
+	 * or unlabeled.
+	 * 
+	 * Edges: Edges are drawn or used to connect two nodes of the graph. It can be
+	 * ordered pair of nodes in a directed graph. Edges can connect any two nodes in
+	 * any possible way. There are no rules. Sometimes, edges are also known as
+	 * arcs. Every edge can be labelled/unlabelled.
+	 */
+
+	static class Edge {
+		int src;
+		int dst;
+		int weight;
+
+		public Edge(int src, int dst, int weight) {
+			this.src = src;
+			this.dst = dst;
+			this.weight = weight;
+		}
+	}
+
+	/*
+	 * This graph implementation is done by Adjacency list method. It is efficient
+	 * for finding neighbors.
+	 */
+
+	public static void main(String[] args) {
+		int v = 4;
+
+		ArrayList<Edge> graph[] = new ArrayList[v];
+
+		createGraph(graph);
+
+		// print the neighbors of 2
+		for (int i = 0; i < graph[2].size(); i++) {
+			System.out.println(graph[2].get(i).dst + " , " + graph[2].get(i).weight);
+		}
+	}
+
+	// un-directed non-weighted graph
+	public static void createGraph(ArrayList<Edge> graph[]) {
+		// to avoid java.lang.NullPointerException
+		for (int i = 0; i < graph.length; i++) {
+			graph[i] = new ArrayList<Edge>();
+		}
+
+		// value initialization
+		graph[0].add(new Edge(0, 2, 2));
+
+		graph[1].add(new Edge(1, 2, 10));
+		graph[1].add(new Edge(1, 3, 0));
+
+		graph[2].add(new Edge(2, 0, 2));
+		graph[2].add(new Edge(2, 1, 10));
+		graph[2].add(new Edge(2, 3, -1));
+
+		graph[3].add(new Edge(3, 1, 0));
+		graph[3].add(new Edge(3, 2, -1));
+	}
+
+}
